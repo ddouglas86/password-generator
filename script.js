58e8bf8dd8156a78f9cee2b2b25c8754dab70550
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-07-11 15:23:39 -0500

    added alerts to define variables

--- script.js
+++ script.js
@@ -8,9 +8,33 @@ var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 var speChar = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-//
+// User input 
 function generatePassword() {
-    
+  var userTotalChar = prompt("How many total characters are needed for your password? Please choose between 8-128");
+
+  if (parseInt(userTotalChar) < 8) {
+    alert("Total number of characters must be at least 8");
+    generatePassword();
+  }
+  else if (parseInt(userTotalChar) > 128) {
+    alert("Maximum number of characters is 128");
+    generatePassword();
+  }
+  else if (isNaN(userTotalChar)) {
+    alert("Please enter numerical digits only");
+    generatePassword();
+  }
+  else {
+    var incUpperCase = confirm("Would you like to include Upper Case letters? \nOK for YES \nCancel for NO");
+
+    var incLowerCase = confirm("Would you like to include Lower Case letters? \nOK for YES \nCancel for NO");
+
+    var incNumbers = confirm("Would you like to include numbers? \nOK for YES \nCancel for NO");
+
+    var incSpecialChar = confirm("Would you like to include special characters? \nOK for YES \nCancel for NO");
+
+  };
+
 }
 
 // Get references to the #generate element
